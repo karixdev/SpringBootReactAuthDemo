df9ae86ef742cd28017fbf3db4cdd5fb39f1f47b
karixdev <kari.klucz@gmail.com>
2022-11-29 18:52:25 +0100

    feat: configure web security

---
+++ backend/src/main/java/com/karixdev/backend/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.karixdev.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+}
