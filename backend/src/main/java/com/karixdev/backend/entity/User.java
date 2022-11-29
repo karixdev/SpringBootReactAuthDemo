6cff4c902f093986dfc29c0922086cc7d1940d7a
karixdev <kari.klucz@gmail.com>
2022-11-29 18:40:23 +0100

    feat: create User entity

---
+++ backend/src/main/java/com/karixdev/backend/entity/User.java
@@ -0,0 +1,69 @@
+package com.karixdev.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(
+        name = "user",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        columnNames = "email",
+                        name = "user_email_unique"
+                )
+        }
+)
+public class User {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+    @Column(
+            name = "id",
+            nullable = false,
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name = "email",
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "password",
+            nullable = false
+    )
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password);
+    }
+}
