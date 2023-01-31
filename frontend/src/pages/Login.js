f06ca4db6d587914fb702cfaade0c53104688e34
karixdev <30869888+karixdev@users.noreply.github.com>
2023-01-31 15:13:47 +0100

    fix: change typo

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -59,7 +59,7 @@ export default function Login() {
 					<label htmlFor="password">Password</label>
 					<input type="password" className="form-control" id="password" placeholder="Password" ref={password} />
         </div>
-        <button type="submit" className="btn btn-primary">Register</button>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
 		</div>
 	)
